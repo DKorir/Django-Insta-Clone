0bd187541487f61fc9652dc166a149283dc42020
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 14:08:21 +0300

    create UserEditView

--- users/views.py
+++ users/views.py
@@ -46,3 +46,11 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
