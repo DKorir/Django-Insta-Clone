aae94721ac123a862300ed857d32e57c22e6ae5a
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 14:07:23 +0300

    create UserRegisterView

--- users/views.py
+++ users/views.py
@@ -41,3 +41,8 @@ class PasswordsChangeView(PasswordChangeView):
 
 def password_success(request):
     return render(request,'registration/password_success.html', {})     
+
+class UserRegisterView(generic.CreateView):
+    form_class = SignUpForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
