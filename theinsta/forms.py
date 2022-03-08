8b22650916decd858f427d95170c761995fc240c
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:31:17 +0300

    create choices for the forms

--- theinsta/forms.py
+++ theinsta/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from . models import Post, Category
+
+choices = Category.objects.all().values_list('name','name')
+choice_list = []
+for item in choices:
+    choice_list.append(item)
