36b8c3fc05365fabc2af824662b17774ce8931c9
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 14:04:09 +0300

    createprofilepageview

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.views.generic import DetailView,CreateView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import  PasswordChangeView
+from django.urls import reverse_lazy
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
+from theinsta.models import Profile
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = 'registration/create_user_profile_page.html'
+    # fields = '__all__'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio','profile_image','website_url','facebook_url','twitter_url','instagram_url','pinterest_url']
+    success_url = reverse_lazy('home')
