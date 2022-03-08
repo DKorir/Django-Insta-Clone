798f4248b1d7334117bf733176e9959f173d426c
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 14:42:20 +0300

    register models in the admin

--- theinsta/admin.py
+++ theinsta/admin.py
@@ -1,3 +1,7 @@
+from cProfile import Profile
 from django.contrib import admin
-from . models import Post
+from . models import Post, Profile, Comment, Category
 admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Profile)
+admin.site.register(Comment)
