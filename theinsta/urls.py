5a5bd21ec914adfeb0e028c4748f37eb151538fb
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:39:31 +0300

    create paths or addpost and article detail

--- theinsta/urls.py
+++ theinsta/urls.py
@@ -5,4 +5,5 @@ from .views import AddCategoryView, AddPostView, ArticleDetailView, CategoryList
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>',ArticleDetailView.as_view(), name="article-detail" ),
+    path('add_post/',AddPostView.as_view(), name="add_post"),
 ]
