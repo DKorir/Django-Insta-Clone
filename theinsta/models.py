bcbdfcae3e74dc9882b10da39e4c63f2d9572e07
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:57:22 +0300

    create comment for the instagram for the user to comment on the post create the model that takes foreign key of the user and the post

--- theinsta/models.py
+++ theinsta/models.py
@@ -21,7 +21,7 @@ class Profile(models.Model):
     twitter_url = models.CharField(max_length=255,null=True, blank=True)
     instagram_url = models.CharField(max_length=255,null=True, blank=True)
     pinterest_url = models.CharField(max_length=255,null=True, blank=True)
-    
+
     def __str__(self):
         return str(self.user)
     def get_absolute_url(self):
@@ -46,3 +46,13 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse("article-detail",args=(str(self.id)))
         return reverse('home')
+class Comment(models.Model):
+     post = models.ForeignKey(Post,related_name="comments" ,on_delete=models.CASCADE)
+     name = models.CharField(max_length=255)
+     body = models.TextField()
+     date_added = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+         return '%s - %s' % (self.post.title, self.name)
+
+
