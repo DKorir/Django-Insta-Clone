966ceb9eb0cbfe5ec78eed3f465e0b91d70a98a5
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 12:51:28 +0300

    create forms in users app

--- insta/urls.py
+++ insta/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theinsta.urls')),
-]
+    # path('users/', include('django.contrib.auth.urls')),
+    # path('users/', include('users.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
