cc0928aa55defc470182b062a001dc181348b8bd
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:32:52 +0300

    create forms for post class model and introduced class meta to overide with the bootstrapified forms using widgets

--- theinsta/forms.py
+++ theinsta/forms.py
@@ -5,3 +5,15 @@ choices = Category.objects.all().values_list('name','name')
 choice_list = []
 for item in choices:
     choice_list.append(item)
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','title_tag','author','category','body','header_image')
+        widgets={
+            'title':forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag':forms.TextInput(attrs={'class': 'form-control'}),
+            'author':forms.TextInput(attrs={'class': 'form-control','value': ' ','id':'elder','type':'hidden'}),
+            # 'author':forms.Select(attrs={'class': 'form-control'}),
+            'category':forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
+            'body':forms.Textarea(attrs={'class': 'form-control'}),
+        }
