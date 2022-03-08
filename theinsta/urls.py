7e4fe0915aeb4a64df2cb398c30ea452243a3f8f
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:46:24 +0300

    create delete post urls in the app theinsta

--- theinsta/urls.py
+++ theinsta/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('article/<int:pk>',ArticleDetailView.as_view(), name="article-detail" ),
     path('add_post/',AddPostView.as_view(), name="add_post"),
     path('add_category/',AddCategoryView.as_view(), name="add_category"),
-     path('article/edit/<int:pk>',UpdatePostView.as_view(), name="update_post"),
+    path('article/edit/<int:pk>',UpdatePostView.as_view(), name="update_post"),
+    path('article/<int:pk>/remove',DeletePostView.as_view(), name="delete_post"),
 ]
