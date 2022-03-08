7c07ac23f514cd1f157b5acbab7f15f283f51115
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:55:41 +0300

    define the path and redirect for the user after the filling of the fields in the profile to home

--- theinsta/models.py
+++ theinsta/models.py
@@ -21,6 +21,12 @@ class Profile(models.Model):
     twitter_url = models.CharField(max_length=255,null=True, blank=True)
     instagram_url = models.CharField(max_length=255,null=True, blank=True)
     pinterest_url = models.CharField(max_length=255,null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.user)
+    def get_absolute_url(self):
+        # return reverse("article-detail",args=(str(self.id)))
+        return reverse('home')
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
