ab01917c5d8a4d0c48f86acda870dea1c5e8338b
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:41:39 +0300

    create addcategory path

--- theinsta/urls.py
+++ theinsta/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>',ArticleDetailView.as_view(), name="article-detail" ),
     path('add_post/',AddPostView.as_view(), name="add_post"),
+    path('add_category/',AddCategoryView.as_view(), name="add_category"),
 ]
