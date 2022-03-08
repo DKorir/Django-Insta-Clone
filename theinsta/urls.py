3bab16edb5a1b1d558f9cb1bddf7000ac232b77f
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:47:23 +0300

    create path for category view in the app theinsta

--- theinsta/urls.py
+++ theinsta/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add_category/',AddCategoryView.as_view(), name="add_category"),
     path('article/edit/<int:pk>',UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove',DeletePostView.as_view(), name="delete_post"),
+    path('category/<str:cats>/', CategoryView, name='category'),
 ]
