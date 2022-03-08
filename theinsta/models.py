7332cd1d79e4d9b08cb56cbf413c56b1a7bde083
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:52:46 +0300

    create profile_image and urls for the image by the user

--- theinsta/models.py
+++ theinsta/models.py
@@ -15,6 +15,8 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255,null=True, blank=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
