5570613279b3510ec1d7ff71511468c386c3c388
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:18:39 +0300

    create article view detail

--- theinsta/views.py
+++ theinsta/views.py
@@ -20,3 +20,18 @@ def CategoryListView(request):
 def CategoryView(request,cats):
     category_posts = Post.objects.filter(category= cats.replace('-',' '))
     return render(request,'categories.html',{'cats': cats.replace('-',' ').title(),'category_posts': category_posts})
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView,self).get_context_data(*args, **kwargs)
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context
