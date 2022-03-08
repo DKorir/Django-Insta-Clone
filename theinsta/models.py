2f7e5541500f4c8d59a92eb4a7b60883767cb916
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 07:47:43 +0300

    creeate model post and title,author,body

--- theinsta/models.py
+++ theinsta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = RichTextField(blank=True, null=True)
+    def __str__(self):
+        return self.title+ " | " + str(self.author)
