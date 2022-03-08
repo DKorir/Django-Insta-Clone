187d7866c7c931601f52153103421daf6aa59dae
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 07:58:20 +0300

    create titletag,header_imagein the post model

--- theinsta/models.py
+++ theinsta/models.py
@@ -6,6 +6,8 @@ from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    title_tag = models.CharField(max_length=255,default="MyBlog!")
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     def __str__(self):
