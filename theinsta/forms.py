e0172505a0df80b14c9ed013ce4d9414916efa66
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 10:34:07 +0300

    create editform for the class model post and placed the fields

--- theinsta/forms.py
+++ theinsta/forms.py
@@ -17,3 +17,15 @@ class PostForm(forms.ModelForm):
             'category':forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
             'body':forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','title_tag','body')
+        widgets={
+            'title':forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag':forms.TextInput(attrs={'class': 'form-control'}),
+            # 'author':forms.Select(attrs={'class': 'form-control'}),
+            'body':forms.Textarea(attrs={'class': 'form-control'}),
+        }
