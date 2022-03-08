e75e6569053e6e4ecc1afcf4a2df04da59f6f843
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 14:05:15 +0300

    create ShowProfilePageView

--- users/views.py
+++ users/views.py
@@ -21,3 +21,14 @@ class EditProfilePageView(generic.UpdateView):
     template_name = 'registration/edit_profile_page.html'
     fields = ['bio','profile_image','website_url','facebook_url','twitter_url','instagram_url','pinterest_url']
     success_url = reverse_lazy('home')
+    
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+    def get_context_data(self, *args, **kwargs):
+        # users = Profile.objects.all()
+        context = super(ShowProfilePageView,self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
+        
