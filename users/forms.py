410b475e870a7870c4d837f3566b5e72269e726d
DKorir <dominic.korir@student.moringaschool.com>
2022-03-08 13:00:00 +0300

    create profile page form

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,22 @@ from django.contrib.auth.models import User
 from django import forms
 from theinsta.models import Profile
 
+class ProfilePageForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio','profile_image','website_url','facebook_url','twitter_url','instagram_url','pinterest_url')
+
+
+        widgets = {
+            'bio':forms.Textarea(attrs={'class': 'form-control'}),
+            # 'profile_image':forms.ImageField(attrs={'class': 'form-control'}),
+            'website_url':forms.TextInput(attrs={'class': 'form-control'}),  
+            'facebook_url':forms.TextInput(attrs={'class': 'form-control'}),  
+            'twitter_url':forms.TextInput(attrs={'class': 'form-control'}),  
+            'instagram_url':forms.TextInput(attrs={'class': 'form-control'}),  
+            'pinterest_url':forms.TextInput(attrs={'class': 'form-control'}),      
+        }
+
 
 class SignUpForm(UserCreationForm):
     email =forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
