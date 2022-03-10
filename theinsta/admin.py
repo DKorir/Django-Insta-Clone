03aad865e6b62af8218e45b468019b4fb599dd87
DKorir <dominic.korir@student.moringaschool.com>
2022-03-10 10:23:23 +0300

    change database

--- theinsta/admin.py
+++
@@ -1,7 +0,0 @@
-from cProfile import Profile
-from django.contrib import admin
-from . models import Post, Profile, Comment, Category
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Profile)
-admin.site.register(Comment)
